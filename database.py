4c125605b7757bd6660c5d47c890a07fe8bc7531
omar0ali <bajunaidomar@gmail.com>
2023-12-29 14:41:24 -0500

    Updated database, and validation input

--- database.py
+++ database.py
@@ -1,6 +1,64 @@
 import sqlite3
-
-
-class database:
+import datetime
+import re
+from rich.console import Console
+from rich.theme import Theme
+class Database:
+    console = Console(theme=Theme({
+        "info": "dim cyan",
+        "warning": "magenta",
+        "danger": "bold red"
+    }))
+    connection = sqlite3.connect('kashfHesab.db')
+    cursor = connection.cursor()
     def __init__(self) -> None:
-        pass
+        
+        ## CREATE TABELS ##
+        # CREATE COMPANY TABLE
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS company (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                connectionDate DATE,
+                description TEXT,
+                email TEXT,
+                phone TEXT CHECK (length(phone) <= 15)                  
+        """)
+        
+        
+        
+        
+        
+        self.cursor.connection.commit()
+
+        
+        
+    def insertCompany(self, name: str, date: str, desc:str, email:str, phone:str):
+        if(self.validate_date(date)==False):
+            return
+        if(self.validate_phone(phone)==False):
+            return
+        self.cursor.execute('''
+            INSERT INTO company (name, connectionDate, description, email, phone) 
+            VALUES (?,?,?,?,?)
+        ''', (name, date, desc, email, phone))
+        self.connection.commit()
+    
+    
+    def validate_date(self, date_str)->bool:
+        try:
+            datetime.strptime(date_str, '%Y-%m-%d')
+            return True
+        except ValueError as e:
+            self.console.print("Validation Failed: "+e, style="danger")
+            return False
+
+    def validate_phone(self, phone_str) -> bool:
+        validate = re.match(r'^\d{10}$', phone_str) is not None
+        if(validate==None):
+            self.console.print("Validation phone number failed.", style="danger")
+            return False
+        return 
+        
+    def disconnect(self):
+        self.connection.close()
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-import database as db
-import re as validateInfo
+from typing import Any
+from database import Database
 import inquirer
-import rich
+from rich.console import Console
 from pyfiglet import Figlet
+import sys, subprocess
 
 class Main:
+    console = Console()
+    db = Database()# Init tables.
     def __init__(self) -> None:
+        self.clear_screen()
         f = Figlet(font='small')
-        print(f.renderText('Account of statement'))  
-        pass
+        print(f.renderText('Account of statement'))
+        self.display_menu() 
+
+    #Display the menu
+    def display_menu(self):
+        self.console.rule("[bold red]Main Menu")
+        while(True):
+            answer = inquirer.prompt(questions=[inquirer.List(
+                name="select", 
+                message="Operations", 
+                choices=["All Accounts",
+                        "Accounts of a company",
+                        "Insert a company",
+                        "Exit"]
+                )])
+            
+            
+            
+            if answer.get('select') == "Exit":
+                self.clear_screen()
+                break
+             
+    
+    
+    #Clearning the screen.
+    def clear_screen(self):
+        os = sys.platform
+        if os == 'win32':
+            subprocess.run('cls', shell=True)
+        elif os == 'darwin' or os == 'linux':
+            subprocess.run('clear', shell=True)
 
 
 
